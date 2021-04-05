53b780cab946fc8270cef8412f23aa84d8ebc9f2
SamStegman <sam1998m@gmail.com>
2021-04-05 15:31:28 -0400

    seeding for most parts. not tested fully

--- backend/pcpp_assistant_backend/pcpp/models.py
+++ backend/pcpp_assistant_backend/pcpp/models.py
@@ -5,14 +5,161 @@ from django.db import models
 
 class CPU(models.Model):
     name = models.CharField(max_length=256)
-    image = models.TextField()    
+    image = models.TextField()   
     minPrice = models.DecimalField(decimal_places=2, max_digits=10)
+
     coreClock = models.FloatField()
     coreCount = models.IntegerField()
     coreFamily = models.CharField(max_length=128)
     manufacturer = models.CharField(max_length=128)
     microarchitecture = models.CharField(max_length=128)
     socket = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Case(models.Model):
+    name = models.CharField(max_length=256)
+    image = models.TextField()
+    minPrice = models.DecimalField(decimal_places=2, max_digits=10)
+
+    Color = models.TextField()
+    Dimensions = models.TextField()
+    External_Bays = models.IntegerField()
+    Front_Panel_USB = models.TextField()
+    Full_Height_Expansion_Slots = models.IntegerField()
+    Half_Height_Expansion_Slots = models.IntegerField()
+    Internal_Twoin_Bays = models.IntegerField()
+    Internal_Threein_Bays = models.IntegerField()
+    Manufacturer = models.TextField()
+    Maximum_Video_Card_Length = models.TextField()
+    Model = models.TextField()
+    Motherboard_Form_Factor = models.TextField()
+    Part_num = models.TextField()
+    Power_Supply = models.TextField()
+    Power_Supply_Shroud = models.TextField()
+    Side_Panel_Window = models.TextField()
+    Type = models.TextField()
+    Volume = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class GPU(models.Model):
+    name = models.CharField(max_length=256)
+    image = models.TextField()
+    minPrice = models.DecimalField(decimal_places=2, max_digits=10)
+
+    Boost_Clock = models.FloatField()
+    Chipset = models.TextField()
+    Color = models.TextField()
+    Cooling = models.TextField()
+    Core_Clock = models.FloatField()
+    Dvi_Ports = models.IntegerField()
+    DVI_D_Dual_Link = models.IntegerField()
+    DisplayPort = models.IntegerField()
+    DisplayPort_Ports = models.IntegerField()
+    Effective_Memory_Clock = models.FloatField()
+    Expansion_Slot_Width = models.TextField()
+    External_Power = models.TextField()
+    Frame_Sync = models.TextField()
+    HDMI_Ports = models.TextField()
+    Interface = models.TextField()
+    Length = models.TextField()
+    Manufacturer = models.TextField()
+    Memory = models.IntegerField()
+    Memory_Type = models.TextField()
+    Mini_DisplayPort_Ports = models.TextField()
+    Mini_HDMI_Ports = models.TextField()
+    Part_num = models.TextField()
+    TDP = models.IntegerField()
+    VGA = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Memory(models.Model):
+    name = models.CharField(max_length=256)
+    image = models.TextField()   
+    minPrice = models.DecimalField(decimal_places=2, max_digits=10)
+
+    CAS_Latency = models.IntegerField()
+    Color = models.TextField()
+    ECC_Registered = models.TextField()
+    First_Word_Latency = models.FloatField()
+    Form_Factor = models.TextField()
+    Heat_Spreader = models.TextField()
+    Manufacturer = models.TextField()
+    Modules = models.TextField()
+    Part_num = models.TextField()
+    Price_GB = models.TextField()
+    Voltage = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Motherboard(models.Model):
+    name = models.CharField(max_length=256)
+    image = models.TextField()
+    minPrice = models.DecimalField(decimal_places=2, max_digits=10)
+
+    Chipset = models.TextField()
+    Color = models.TextField()
+    Form_Factor = models.TextField()
+    Manufacturer = models.TextField()
+    Memory_Max = models.IntegerField()
+    Memory_Slots = models.IntegerField()
+    Memory_Speed = models.TextField()
+    Memory_Type = models.TextField()
+    Model = models.TextField()
+    Onboard_Ethernet = models.TextField()
+    Onboard_Video = models.TextField()
+    PCI_Slots = models.IntegerField()
+    PCI_E_one_Slots = models.IntegerField()
+    PCI_E_sixteen_Slots = models.IntegerField()
+    PCI_E_four_Slots = models.IntegerField()
+    PCI_E_eight_Slots = models.IntegerField()
+    Part_num = models.TextField()
+    RAID_Support = models.TextField()
+    SATA_three_Gbs = models.IntegerField()
+    SATA_six_Gbs = models.IntegerField()
+    Socket_CPU = models.TextField()
+    Supports_ECC = models.TextField()
+    USB_two_Headers = models.IntegerField()
+    USB_three_Gen_one_Headers = models.IntegerField()
+    USB_three_Gen_two_Headers = models.IntegerField()
+    USB_three_Gen_twoxtwo_Headers = models.IntegerField()
+    Wireless_Networking = models.TextField()
+    eSATA_threeGbs = models.IntegerField()
+    mSATA_Slots = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class power(models.Model):
+    name = models.CharField(max_length=256)
+    image = models.TextField()
+    minPrice = models.DecimalField(decimal_places=2, max_digits=10)
+
+    Color = models.TextField()
+    EPS_Connectors = models.IntegerField()
+    Efficiency = models.TextField()
+    Efficiency_Rating = models.TextField()
+    Fanless = models.TextField()
+    Form_Factor = models.TextField()
+    Length = models.TextField()
+    Manufacturer = models.TextField()
+    Modular = models.TextField()
+    Molex_fourPin_Connectors = models.IntegerField()
+    Output = models.TextField()
+    PCIe_sixtwoPin_Connectors = models.IntegerField()
+    Part_num = models.TextField()
+    SATA_Connectors = models.IntegerField()
+    Type = models.TextField()
+    Wattage = models.IntegerField()
 
     def __str__(self):
         return self.name
@@ -24,3 +171,5 @@ class Price(models.Model):
 
     def __str__(self):
         return str(self.price)
+
+
--- backend/pcpp_assistant_backend/pcpp/views.py
+++ backend/pcpp_assistant_backend/pcpp/views.py
@@ -4,7 +4,7 @@ from .serializers import HardwearSerializer
 from rest_framework.response import Response
 from django.http import HttpResponse
 import json
-from .models import Price, CPU
+from .models import Price, CPU, Case, GPU, Memory, Motherboard, power
 
 # Create your views here.
 
@@ -21,28 +21,6 @@ class SeedData(views.APIView):
 
         return HttpResponse("hello")
 
-
-def seedCPU():
-    f = open(r'../../backend/data/cpu_data.json',)
-    data = json.load(f)
-    print(type(data))
-    for name, info in data.items():
-        price = info["buying_info"]
-        part_info = info["part_info"]
-
-        c = CPU(name=name, image="missing", minPrice=min_price(price),
-                coreClock=process_num(part_info["Core Clock"], "GHz"), coreCount=process_num(part_info["Core Count"], ""),
-                coreFamily=part_info["Core Family"], manufacturer=part_info["Manufacturer"],
-                microarchitecture=part_info["Microarchitecture"], socket=part_info["Socket"])
-        c.save()        
-        
-        if len(price.items()) != 0:
-            for link, p in price.items():
-                cost = float(p.strip("$").strip("+"))
-                p = Price(price=cost, link=link, item = c)
-                p.save()
-    return "Seeded CPU"
-
 def min_price(p):
     minp = None
     if len(p.items()) == 0:
@@ -56,3 +34,224 @@ def min_price(p):
 
 def process_num(num, split_string):
     return float(num.strip(split_string))
+
+def process_field(info, field):
+    if info.has_key(field):
+        return info[field]
+    else:
+        return None
+
+
+def seedCPU():
+    f = open(r'../../backend/data/good_data/cpu_data.json',)
+    data = json.load(f)
+    print(type(data))
+    for name, info in data.items():
+        price = info["buying_info"]
+        part_info = info["part_info"]
+
+        c = CPU(name=name, image="missing", minPrice=min_price(price),
+                coreClock=process_num(process_field(part_info, ""), "GHz"), 
+                coreCount=process_num(process_field(part_info, ""), ""),
+                coreFamily=process_field(part_info, ""),
+                manufacturer=process_field(part_info, ""),
+                microarchitecture=process_field(part_info, ""),
+                socket=process_field(part_info, ""))
+        c.save()
+
+        if len(price.items()) != 0:
+            for link, p in price.items():
+                cost = float(p.strip("$").strip("+"))
+                p = Price(price=cost, link=link, item=c)
+                p.save()
+    return "Seeded CPU"
+
+
+def seedCase():
+    f = open(r'../../backend/data/good_data/case_data.json',)
+    data = json.load(f)
+    print(type(data))
+    for name, info in data.items():
+        price = info["buying_info"]
+        part_info = info["part_info"]
+
+        c = Case(name=name, image="missing", minPrice=min_price(price),
+            Color=process_field(part_info, "Color"),
+            Dimensions=process_field(part_info, "Dimensions"),
+            External_Bays=process_field(part_info, "External 5.25\" Bays"),
+            Front_Panel_USB=process_field(part_info, "Front Panel USB"),
+            Full_Height_Expansion_Slots=process_num(process_field(part_info, "Full-Height Expansion Slots"), ""),
+            Half_Height_Expansion_Slots=process_num(process_field(part_info, "Half-Height Expansion Slots"), ""),
+            Internal_Twoin_Bays=process_num(process_field(part_info, "Internal 2.5\" Bays"), ""),
+            Internal_Threein_Bays=process_num(process_field(part_info, "Internal 3.5\" Bays"), ""),
+            Manufacturer=process_field(part_info, "Manufacturer"),
+            Maximum_Video_Card_Length=process_field(part_info, "Maximum Video Card Length"),
+            Model=process_field(part_info, "Model"),
+            Motherboard_Form_Factor=process_field(part_info, "Motherboard Form Factor"),
+            Part_num=process_field(part_info, "Part #"),
+            Power_Supply=process_field(part_info, "Power Supply"),
+            Power_Supply_Shroud=process_field(part_info, "Power Supply Shroud"),
+            Side_Panel_Window=process_field(part_info, "Side Panel Window"),
+            Type=process_field(part_info, "Type"),
+            Volume=process_field(part_info, "Volume"))
+        c.save()
+
+        if len(price.items()) != 0:
+            for link, p in price.items():
+                cost = float(p.strip("$").strip("+"))
+                p = Price(price=cost, link=link, item=c)
+                p.save()
+    return "Seeded CASE"
+
+def seedGPU():
+    f = open(r'../../backend/data/good_data/gpu_data.json',)
+    data = json.load(f)
+    print(type(data))
+    for name, info in data.items():
+        price = info["buying_info"]
+        part_info = info["part_info"]
+
+        c = GPU(name=name, image="missing", minPrice=min_price(price),
+            Boost_Clock = process_num(process_field(part_info, "Boost Clock"), "MHz"),
+            Chipset = process_field(part_info, "Chipset"),
+            Color = process_field(part_info, "Color"),
+            Cooling = process_field(part_info, "Cooling"),
+            Core_Clock = process_num(process_field(part_info, "Core Clock"), "MHz"),
+            Dvi_Ports = process_num(process_field(part_info, "DVI Ports"), ""),
+            DVI_D_Dual_Link = process_num(process_field(part_info, "DVI-D Dual-Link"), ""),
+            DisplayPort = process_num(process_field(part_info, "DisplayPort"), ""),
+            DisplayPort_Ports = process_num(process_field(part_info, "DisplayPort Ports"), ""),
+            Effective_Memory_Clock = process_num(process_field(part_info, "Effective Memory Clock"), "MHz"),
+            Expansion_Slot_Width = process_field(part_info, "Expansion Slot Width"),
+            External_Power = process_field(part_info, "External Power"),
+            Frame_Sync = process_field(part_info, "Frame Sync"),
+            HDMI_Ports = process_field(part_info, "HDMI Ports"),
+            Interface = process_field(part_info, "Interface"),
+            Length = process_field(part_info, "Length"),
+            Manufacturer = process_field(part_info, "Manufacturer"),
+            Memory = process_num(process_field(part_info, "Memory"), ""),
+            Memory_Type = process_field(part_info, "Memory Type"),
+            Mini_DisplayPort_Ports = process_field(part_info, "Mini-DisplayPort Ports"),
+            Mini_HDMI_Ports = process_field(part_info, "Mini-HDMI Ports"),
+            Part_num = process_field(part_info, "Part #"),
+            TDP = process_num(process_field(part_info, "TDP"), "W"),
+            VGA = process_field(part_info, "VGA"))
+        c.save()
+
+        if len(price.items()) != 0:
+            for link, p in price.items():
+                cost = float(p.strip("$").strip("+"))
+                p = Price(price=cost, link=link, item=c)
+                p.save()
+    return "Seeded GPU"
+
+def seedMemory():
+    f = open(r'../../backend/data/good_data/memory_data.json',)
+    data = json.load(f)
+    print(type(data))
+    for name, info in data.items():
+        price = info["buying_info"]
+        part_info = info["part_info"]
+
+        c = Memory(name=name, image="missing", minPrice=min_price(price),
+                CAS_Latency = process_num(process_field(part_info, "CAS Latency"), ""),
+                Color = process_field(part_info, "Color"),
+                ECC_Registered = process_field(part_info, "ECC / Registered"),
+                First_Word_Latency = process_num(process_field(part_info, "First Word Latency"), "ns"),
+                Form_Factor = process_field(part_info, "Form Factor"),
+                Heat_Spreader = process_field(part_info, "Heat Spreader"),
+                Manufacturer = process_field(part_info, "Manufacturer"),
+                Modules = process_field(part_info, "Modules"),
+                Part_num = process_field(part_info, "Part #"),
+                Price_GB = process_field(part_info, "Price / GB"),
+                Voltage = process_field(part_info, "Voltage"))
+        c.save()
+
+        if len(price.items()) != 0:
+            for link, p in price.items():
+                cost = float(p.strip("$").strip("+"))
+                p = Price(price=cost, link=link, item=c)
+                p.save()
+    return "Seeded memory"
+
+
+def seedMboard():
+    f = open(r'../../backend/data/good_data/motherboard_data.json',)
+    data = json.load(f)
+    print(type(data))
+    for name, info in data.items():
+        price = info["buying_info"]
+        part_info = info["part_info"]
+
+        c = Motherboard(name=name, image="missing", minPrice=min_price(price),
+            Chipset = process_field(part_info, "Chipset"),
+            Color = process_field(part_info, "Color"),
+            Form_Factor = process_field(part_info, "Form Factor"),
+            Manufacturer = process_field(part_info, "Manufacturer"),
+            Memory_Max = process_num(process_field(part_info, "Memory Max"), "GB"),
+            Memory_Slots = process_num(process_field(part_info, "Memory Slots"), ""),
+            Memory_Speed = process_field(part_info, "Memory Speed"),
+            Memory_Type = process_field(part_info, "Memory Type"),
+            Model = process_field(part_info, "Model"),
+            Onboard_Ethernet = process_field(part_info, "Onboard Ethernet"),
+            Onboard_Video = process_field(part_info, "Onboard Video"),
+            PCI_Slots = process_num(process_field(part_info, "PCI Slots"), ""),
+            PCI_E_one_Slots = process_num(process_field(part_info, "PCI-E x1 Slots"), ""),
+            PCI_E_sixteen_Slots = process_num(process_field(part_info, "PCI-E x16 Slots"), ""),
+            PCI_E_four_Slots = process_num(process_field(part_info, "PCI-E x4 Slots"), ""),
+            PCI_E_eight_Slots = process_num(process_field(part_info, "PCI-E x8 Slots"), ""),
+            Part_num = process_field(part_info, "Part #"),
+            RAID_Support = process_field(part_info, "RAID Support"),
+            SATA_three_Gbs = process_num(process_field(part_info, "SATA 3 Gb/s"), ""),
+            SATA_six_Gbs = process_num(process_field(part_info, "SATA 6 Gb/s"), ""),
+            Socket_CPU = process_field(part_info, "Socket / CPU"),
+            Supports_ECC = process_field(part_info, "Supports ECC"),
+            USB_two_Headers = process_num(process_field(part_info, "USB 2.0 Headers"), ""),
+            USB_three_Gen_one_Headers = process_num(process_field(part_info, "USB 3.2 Gen 1 Headers"), ""),
+            USB_three_Gen_two_Headers = process_num(process_field(part_info, "USB 3.2 Gen 2 Headers"), ""),
+            USB_three_Gen_twoxtwo_Headers = process_num(process_field(part_info, "USB 3.2 Gen 2x2 Headers"), ""),
+            Wireless_Networking = process_field(part_info, "Wireless Networking"),
+            eSATA_threeGbs = process_num(process_field(part_info, "eSATA 3Gb/s"), ""),
+            mSATA_Slots = process_num(process_field(part_info, "mSATA Slots"), ""))
+        c.save()
+
+        if len(price.items()) != 0:
+            for link, p in price.items():
+                cost = float(p.strip("$").strip("+"))
+                p = Price(price=cost, link=link, item=c)
+                p.save()
+    return "Seeded M Board"
+
+def seedPower():
+    f = open(r'../../backend/data/good_data/memory_data.json',)
+    data = json.load(f)
+    print(type(data))
+    for name, info in data.items():
+        price = info["buying_info"]
+        part_info = info["part_info"]
+
+        c = power(name=name, image="missing", minPrice=min_price(price),
+                Color = process_field(part_info, "Color"),
+                EPS_Connectors = process_num(process_field(part_info, "EPS Connectors"), ""),
+                Efficiency = process_field(part_info, "Efficiency"),
+                Efficiency_Rating = process_field(part_info, "Efficiency Rating"),
+                Fanless = process_field(part_info, "Fanless"),
+                Form_Factor = process_field(part_info, "Form Factor"),
+                Length = process_field(part_info, "Length"),
+                Manufacturer = process_field(part_info, "Manufacturer"),
+                Modular = process_field(part_info, "Modular"),
+                Molex_fourPin_Connectors = process_num(process_field(part_info, "Molex 4-Pin Connectors"), ""),
+                Output = process_field(part_info, "Output"),
+                PCIe_sixtwoPin_Connectors = process_num(process_field(part_info, "PCIe 6+2-Pin Connectors"), ""),
+                Part_num = process_field(part_info, "Part #"),
+                SATA_Connectors = process_num(process_field(part_info, "SATA Connectors"), ""),
+                Type = process_field(part_info, "Type"),
+                Wattage = process_num(process_field(part_info, "Wattage"), "W"))
+        c.save()
+
+        if len(price.items()) != 0:
+            for link, p in price.items():
+                cost = float(p.strip("$").strip("+"))
+                p = Price(price=cost, link=link, item=c)
+                p.save()
+    return "Seeded power"
