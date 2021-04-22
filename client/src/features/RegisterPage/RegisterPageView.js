9437d766f20e4a30b80f1c5a4c464c44196526fa
Caleb Goddard <goddard.103@osu.edu>
2021-04-22 02:30:37 -0400

    added textfields for sign in and create account

--- client/src/features/RegisterPage/RegisterPageView.js
+++ client/src/features/RegisterPage/RegisterPageView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from 'components/Header';
-import { Box, Card, Typography } from '@material-ui/core';
+import { Box, TextField, Typography, Button } from '@material-ui/core';
 import useStyles from './styles';
 
 const RegisterPageView = () => {
@@ -10,7 +10,32 @@ const RegisterPageView = () => {
         <Box className={styles.container}>
             <Header />
             <Box className={styles.innerContainer}>
-                <Box className={styles.optionsContainer}></Box>
+                <Box className={styles.optionsContainer}>
+                    <Typography className={styles.subtext}>Sign in.</Typography>
+                    <TextField
+                        label="Username"
+                        className={styles.textField}
+                    ></TextField>
+                    <TextField
+                        label="Password"
+                        className={styles.textField}
+                    ></TextField>
+                    <Button variant="contained" className={styles.submitButton}>Submit</Button>
+                    <Typography className={styles.subtext}>
+                        Create an account.
+                    </Typography>
+                    <TextField label="Username" className={styles.textField}>Username</TextField>
+                    <TextField label="Email" className={styles.textField}>Email</TextField>
+                    <TextField label="First Name" className={styles.textField}>
+                        First Name
+                    </TextField>
+                    <TextField label="Last Name" className={styles.textField}>
+                        Last Name
+                    </TextField>
+                    <TextField label="Password" className={styles.textField}>Password</TextField>
+                    <TextField label="Confirm Password" className={styles.textField}>Password2</TextField>
+                    <Button variant="contained" className={styles.submitButton}>Submit</Button>
+                </Box>
             </Box>
 
         </Box>
